7dd0514649d2ba99cedc98716967b850c310aa05
Nate <nathanperfetti@gmail.com>
2019-06-30 22:27:48 -0500

    implemented control flow even nums

--- timer.py
+++ timer.py
@@ -1,9 +1,10 @@
 import time 
 def countdown(n):
     while n > 0:
+        n = n - 1
         print(n)
         time.sleep(1)
-        n = n - 1
-        if n == 0 :
-            print('Done-Zo!')
+        
+        if n % 2 == 0 :
+            print('Every oudder! ' ,n)
 countdown(50)
