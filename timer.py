18fa1c0bd2030bf554eb954206bdb66ebd0225f2
Nate <nathanperfetti@gmail.com>
2019-07-01 17:38:13 -0500

    timer and template literalling for python 2.7

--- timer.py
+++ timer.py
@@ -1,10 +1,24 @@
 import time 
 def countdown(n):
+
+    print('Ready! . . . ')
+    time.sleep(1)
+    print('. . . Set!')
+
     while n > 0:
         n = n - 1
+        time.sleep(2)
         print(n)
-        time.sleep(1)
+        
         
         if n % 2 == 0 :
-            print('Every oudder! ' ,n)
-countdown(50)
+             print("Printin' all da evens indicies: %s." %n)
+    
+    if n == 0 :
+        print('. . . ')
+
+        time.sleep(3)
+        print('***** =====================  *****')
+        print('***** 100 DAYS OF CODE 2K19! *****')
+        print('***** =====================  *****')
+countdown(6)
